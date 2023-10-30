400823949be3047688b647676f042117d252f1c9
Tseleng <136009318+Tseleng01@users.noreply.github.com>
2023-10-30 13:21:34 +0200

    Create 1-create_file.c
    Signed-off-by: Tseleng <136009318+Tseleng01@users.noreply.github.com>

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - creates a file
+ * @filename: is the name of the file to be created
+ * @text_content: is a NULL terminated string
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t b_w;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		b_w = write(file, text_content, strlen(text_content));
+		if (b_w == -1)
+		{
+			close(file);
+			return (-1);
+		}#include "main.h"
+#include <string.h>
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: is the name of the file
+ * @text_content: is the NULL terminated string
+ * Return: 1 if success, -1 if failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int f;
+	ssize_t b_w;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	f = open(filename, O_WRONLY | O_APPEND);
+	if (f == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		b_w = write(f, text_content, strlen(text_content));
+		if (b_w == -1)
+		{
+			close(f);
+			return (-1);
+		}
+	}
+
+	close(f);
+	return (1);
+}
