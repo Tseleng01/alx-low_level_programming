517c518fc73d20c16418726ac6d0da8c51a95fc7
Tseleng <136009318+Tseleng01@users.noreply.github.com>
2023-11-01 07:23:28 +0200

    Create 100-get_endianness.c
    Signed-off-by: Tseleng <136009318+Tseleng01@users.noreply.github.com>

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int endianness = 1;
+	char *n;
+
+	n = (char *)&endianness;
+
+	if (*n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
