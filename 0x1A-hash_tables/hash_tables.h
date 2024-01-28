b9a06ffca8b8b4fce95c394c834c0a6a26d92b9a
Tseleng <136009318+Tseleng01@users.noreply.github.com>
2024-01-28 20:44:17 +0200

    Update hash_tables.h
    Signed-off-by: Tseleng <136009318+Tseleng01@users.noreply.github.com>

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -162,3 +162,38 @@ void shash_table_delete(shash_table_t *ht);
 
 #endif /*_HASH_TABLES_H_*/
 
+________________________________
+
+0-main.c
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <stdio.h>
+
+#include "hash_tables.h"
+
+/**
+
+ * main - check the code for
+
+ *
+
+ * Return: Always EXIT_SUCCESS.
+
+ */
+
+int main(void)
+
+{
+
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+
+    printf("%p\n", (void *)ht);
+
+    return (EXIT_SUCCESS);
+
+}
